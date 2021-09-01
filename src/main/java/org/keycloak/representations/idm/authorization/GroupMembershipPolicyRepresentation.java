9d063949532bcc389f922f4e7dd2a5079943109c
Lorenzo Rossoni <lorenzo.rossoni@outlook.com>
2021-09-01 10:44:58 +0200

    up

--- src/main/java/org/keycloak/representations/idm/authorization/GroupMembershipPolicyRepresentation.java
+++ src/main/java/org/keycloak/representations/idm/authorization/GroupMembershipPolicyRepresentation.java
@@ -1,14 +1,5 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package org.keycloak.representations.idm.authorization;
 
-/**
- *
- * @author loren
- */
 public class GroupMembershipPolicyRepresentation extends AbstractPolicyRepresentation {
     private String groupsClaim;
     
